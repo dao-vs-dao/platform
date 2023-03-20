3667ca4f6be76abe441067e04ea1046b13b5af34
0xSwego <OxSwego@gmail.com>
2023-03-20 19:31:16 +0000

    Update URLs

--- dapp/src/services/backend-url-fetcher.ts
+++ dapp/src/services/backend-url-fetcher.ts
@@ -3,13 +3,13 @@ const isTest = typeof window === "undefined";
 const isDev = !isTest && window.location.href.includes("localhost");
 
 const LOCAL_BACKEND_URL = "http://localhost:5000/api";
-const PROD_BACKEND_URL = "http://localhost:5000/api";
+const PROD_BACKEND_URL = "https://backend.daovsdao.xyz/api";
 
 /** The backend API endpoint URL */
 export const backendUrl = isTest || isDev ? LOCAL_BACKEND_URL : PROD_BACKEND_URL;
 
 const LOCAL_BACKEND_WS = "ws://localhost:5000/";
-const PROD_BACKEND_WS = "ws://localhost:5000/";
+const PROD_BACKEND_WS = "ws://backend.daovsdao.xyz/";
 
 /** The backend WSS endpoint URL */
 export const wssBackendUrl = isTest || isDev ? LOCAL_BACKEND_WS : PROD_BACKEND_WS;
