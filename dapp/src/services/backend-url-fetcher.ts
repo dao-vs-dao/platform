dd8d293f3cf4837de708bd9c474e7bcc9ae21293
0xSwego <OxSwego@gmail.com>
2023-03-27 00:06:03 +0100

    Connect over secure WS on prod

--- dapp/src/services/backend-url-fetcher.ts
+++ dapp/src/services/backend-url-fetcher.ts
@@ -9,7 +9,7 @@ const PROD_BACKEND_URL = "https://backend.daovsdao.xyz/api";
 export const backendUrl = isTest || isDev ? LOCAL_BACKEND_URL : PROD_BACKEND_URL;
 
 const LOCAL_BACKEND_WS = "ws://localhost:5000/";
-const PROD_BACKEND_WS = "ws://backend.daovsdao.xyz/";
+const PROD_BACKEND_WS = "wss://backend.daovsdao.xyz/";
 
 /** The backend WSS endpoint URL */
 export const wssBackendUrl = isTest || isDev ? LOCAL_BACKEND_WS : PROD_BACKEND_WS;
