e407a66969c420a76dfbfd2a59e0e9d62ff562e7
0xSwego <OxSwego@gmail.com>
2023-03-26 14:03:37 +0100

    Make sponsorship panel more responsive

--- dapp/src/components/certificates-panel/index.tsx
+++ dapp/src/components/certificates-panel/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { ISponsorshipCertificate } from "../../@types/i-sponsoring";
@@ -36,17 +36,33 @@ const ClosedCertificatesPanel = () => {
 
 const OpenCertificatesPanel = () => {
     const dispatch = useDispatch();
+    const isModalOpen = useSelector((state: RootState) => state.sponsoring.isModalOpen);
     const owned = useSelector((state: RootState) => state.sponsoring.ownedCertificates);
     const beneficiary = useSelector((state: RootState) => state.sponsoring.beneficiaryCertificates);
     const open = owned.filter(cert => !cert.closed);
     const closed = owned.filter(cert => cert.closed);
+    const ref: MutableRefObject<any> = useRef(null);
 
     const sum = (n: number[]) => n.reduce((prev, curr) => curr + prev, 0);
     const calculateCost = (certs: ISponsorshipCertificate[]) => sum(certs.map(c => c.amount));
     const calculateRedeemed = (certs: ISponsorshipCertificate[]) => sum(certs.map(c => c.redeemed));
     const closePanel = () => dispatch(closeSponsoringModal({}));
 
-    return <div className="certificate-panel">
+    useEffect(() => {
+        const handleClickOutside = (event: any) => {
+            if (isModalOpen && ref.current
+                && !event.target?.className?.includes?.("certificate-panel")
+                && !ref.current.contains(event.target)) {
+                closePanel();
+            }
+        };
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
+    return <div className="certificate-panel" ref={ref}>
         <div className="certificate-panel__close" onClick={closePanel} />
         <div className="certificate-panel__title">Sponsorships</div>
         <div className="certificate-panel__description">
@@ -64,8 +80,8 @@ const OpenCertificatesPanel = () => {
                         {open.map(cert => <SponsorshipCertificate key={cert.id} cert={cert} />)}
                     </div>
                     <div className="certificate-panel__stats">
-                        <div>Invested: {calculateCost(closed)} DVD</div>
-                        <div>Redeemable now: {calculateRedeemed(closed)} DVD</div>
+                        <div>Invested: {calculateCost(open)} DVD</div>
+                        <div>Redeemable now: {calculateRedeemed(open)} DVD</div>
                     </div>
                 </>
                 : <div className="certificate-panel__list-msg">You are not sponsoring anyone</div>
